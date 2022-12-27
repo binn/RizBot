3b5791ffc34bd4fd32048d07b6d5cae00f2fb690
Sarmad Wahab <me@bin.moe>
2022-12-27 12:40:21 -0600

    Add project files.

---
+++ RizBot/Data/RizContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace RizBot.Data
+{
+    public class RizContext : DbContext
+    {
+        public RizContext(DbContextOptions<RizContext> options) : base(options) { }
+        
+        public DbSet<Channel> Servers { get; set; }
+    }
+
+    public class Channel
+    {
+        [Key]
+        public string Id { get; set; } = default!;
+    }
+}
---
+++ RizBot/Program.cs
@@ -0,0 +1,71 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RizBot.Data;
+using System.Reflection;
+
+namespace RizBot
+{
+    public class Program
+    {
+        private DiscordSocketClient? _client;
+        private InteractionService? _interactions;
+        private IConfiguration? _configuration;
+        private IServiceProvider? _services;
+        private ILogger? _logger;
+
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true)
+                .AddEnvironmentVariables("RIZ")
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+                .Build();
+
+            var config = new DiscordSocketConfig()
+            {
+                GatewayIntents = GatewayIntents.Guilds,
+            };
+
+            var services = new ServiceCollection();
+            _client = new DiscordSocketClient(config);
+            _interactions = new InteractionService(_client);
+
+            services.AddSingleton(_configuration);
+            services.AddLogging(c => c.AddConfiguration(_configuration).AddConsole());
+            services.AddSingleton(_client);
+            services.AddSingleton(_interactions);
+            services.AddDbContext<RizContext>(o => o.UseNpgsql(_configuration.GetConnectionString("Main")));
+            
+            _services = services.BuildServiceProvider();
+            
+            var scope = _services.CreateScope();
+            _logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+            _client.Log += Log;
+            _client.Ready += Ready;
+
+            await _client.LoginAsync(TokenType.Bot, _configuration["Token"]);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Ready()
+        {
+            _logger!.LogInformation("Bot ready!");
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            _logger!.Log(LogLevel.Information, msg.Exception, msg.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
